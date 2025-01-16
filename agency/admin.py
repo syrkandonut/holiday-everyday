73df919cd2c43b47301ae218aaca577a1b74d0ff
syrkandonut <jacob.888@mail.ru>
2025-01-16 17:41:24 +0300

    ✨  Register models in Admin

--- agency/admin.py
+++ agency/admin.py
@@ -1,1 +1,14 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Feedback, Image, Media, Project, Review, Tag
+
+admin.site.register(
+    (
+        Tag,
+        Project,
+        Review,
+        Feedback,
+        Media,
+        Image,
+    )
+)
