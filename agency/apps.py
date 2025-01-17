1c2f91c9794af4ab3c17776f15120bbde2102c23
syrkandonut <jacob.888@mail.ru>
2025-01-17 17:56:25 +0300

    ♻️  Rename the Proj in the AppsConfig

--- agency/apps.py
+++ agency/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class AgencyConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "agency"
+    verbose_name = "Holiday Everyday"
