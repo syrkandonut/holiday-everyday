39d0c92bb7aca1d4faaffd2123e58e3f1d5b6c14
syrkandonut <jacob.888@mail.ru>
2025-01-23 00:15:06 +0300

    ♻️ Move review spaces to serializer

--- agency/models/review.py
+++ agency/models/review.py
@@ -20,23 +20,3 @@ class Review(Base):
 
     def __str__(self):
         return f"Отзыв проекта {self.project}"
-    
-    def convert_text(self, text: str) -> str:
-        text_list = list(text)  # Преобразуем строку в список для изменения
-        for i in range(len(text_list)):
-            if text_list[i] == " ":
-                for j in range(1, 4):
-                    try:
-                        if i - j >= 0 and text_list[i - j] == " ":
-                            text_list[i - j] = "&nbsp;"
-                            break
-                    except Exception:
-                        continue                  
-        return "".join(text_list) 
-
-        
-    def save(self, *args, **kwargs):
-        self.text = self.convert_text(text=self.text)
-
-        return super().save(*args, **kwargs)
-    
--- agency/serializers/review.py
+++ agency/serializers/review.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from agency.models import Review
 
@@ -7,3 +7,26 @@ class ReviewSerializer(ModelSerializer):
     class Meta:
         model = Review
         fields = ["text"]
+
+    text = SerializerMethodField()
+
+    def convert_text(self, text: str) -> str:
+        new_text = str()
+        for i in range(len(text)):
+            if text[i] == " ":
+                for j in range(1, 4):
+                    attempt = 0
+                    if i - j >= 0 and text[i - j] == " ":
+                        new_text += "&nbsp;"
+                        attempt += 1
+                    
+                if attempt == 0:
+                    new_text += " "
+            else:
+                new_text += text[i]
+                    
+        return new_text
+
+
+    def get_text(self, obj: Review):
+        return self.convert_text(obj.text) 
