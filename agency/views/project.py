972bf7584f726059c78c46a2029d4e813898d3a6
syrkandonut <jacob.888@mail.ru>
2025-01-26 00:47:43 +0300

    🔒️Close unpublished projects in the ProjectView

--- agency/views/project.py
+++ agency/views/project.py
@@ -6,9 +6,18 @@ from agency.serializers.project import ProjectOneSerializer, ProjectSerializer
 
 
 class ProjectViewSet(ModelViewSet):
-    queryset = Project.objects.filter(published=True)
+    queryset = Project.objects.all()
     serializer_class = ProjectSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        if self.action == "list":
+            return Project.objects.filter(published=True)
+
+        if self.action == "retrieve":
+            return Project.objects.all()
+
+        return Project.objects.filter(published=True)
 
     def get_serializer_class(self):
         if self.action == "list":
