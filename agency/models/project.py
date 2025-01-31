2481fde95d8bc15865cdf4fc88423ff39f4b0f74
syrkandonut <jacob.888@mail.ru>
2025-01-31 15:41:19 +0300

    ♻️ Image converter as util

--- agency/models/project.py
+++ agency/models/project.py
@@ -1,8 +1,3 @@
-import io
-import os
-
-import PIL.Image
-from django.core.files.base import ContentFile
 from django.db.models import (
     BooleanField,
     CharField,
@@ -13,7 +8,7 @@ from django.db.models import (
 )
 
 from agency.common.validators.video import rutube_url_validator
-from agency.models.image import IMG_BIG_SIZE, IMG_SMALL_SIZE
+from agency.utils.img_converter import to_webp_and_save_thumbnail
 from config.settings import STORAGE_IMAGE_PATH
 
 from .base import Base
@@ -74,28 +69,7 @@ class Project(Base):
         self.video = self.video.replace("?" + share_postfix, str())
         self.video = self.video.replace("r=plwd", str())
 
-        image_pil = PIL.Image.open(self.preview_image)
-
-        image_io = io.BytesIO()
-
-        image_pil.thumbnail(IMG_BIG_SIZE)
-        image_pil.save(image_io, format="WEBP")
-        image_io.seek(0)
-
-        image_name = f"{os.path.splitext(self.preview_image.name)[0]}.webp"
-        image_content_file = ContentFile(image_io.read())
-
-        self.preview_image.save(image_name, image_content_file, save=False)
-
-        image_io_thumb = io.BytesIO()
-        image_pil.thumbnail(IMG_SMALL_SIZE)
-        image_pil.save(image_io_thumb, format="WEBP")
-        image_io_thumb.seek(0)
-
-        image_name_thumb = f"{os.path.splitext(self.preview_image.name)[0]}_144p.webp"
-
-        with open(image_name_thumb, "wb") as f:
-            f.write(image_io_thumb.read())
+        to_webp_and_save_thumbnail(self.preview_image)
 
         super().save(*args, **kwargs)
 
