ef3c1ea71e686cf8e3e513f2525722f8894f6a4a
syrkandonut <jacob.888@mail.ru>
2025-02-07 17:57:58 +0300

    🎨Preview in the ImageInline

--- agency/admin/inlines.py
+++ agency/admin/inlines.py
@@ -1,14 +1,33 @@
 from django.contrib.admin import StackedInline, TabularInline
+from django.utils.html import format_html
 
 from agency.models import Image, Review
+from config.settings import IMAGE_URL, SERVER_URI, STORAGE_IMAGE_PATH
 
 
 class ImageInLine(TabularInline):
     model = Image
     extra = 0
-    fields = ["name"]
+    fields = ["name", "get_thumbnail"]
+    ordering = ["-name"]
+
+    readonly_fields = ["get_thumbnail"]
+
+    def get_thumbnail(self, obj):
+        if obj.name:
+            image_url = (
+                f"{SERVER_URI}{IMAGE_URL}"
+                + f"{obj.name.name.replace(STORAGE_IMAGE_PATH, str())}"
+            )
+            return format_html(
+                '<img src="{}" style="width: 150px; height: auto;" />', image_url
+            )
+        return ""
+
+    get_thumbnail.short_description = "Картинка"  # type: ignore
 
 
+# Deprecated 07.02.2025 according to the requirements
 class ReviewInLine(StackedInline):
     model = Review
 
