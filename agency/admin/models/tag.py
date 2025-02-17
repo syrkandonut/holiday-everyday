51d93fe71c4a9225dae0f84c648b8184493c067b
Сыроквасов  Данила Владимирович <syrokvasov@bolid.ru>
2025-02-17 11:08:17 +0300

    ✨Prohibit adding tags to ProjectAdmin

--- agency/admin/models/tag.py
+++ agency/admin/models/tag.py
@@ -3,3 +3,6 @@ from django.contrib import admin
 
 class TagAdmin(admin.ModelAdmin):
     fields = ["name"]
+
+    def has_add_permission(self, request):
+        return "project" not in request.path
